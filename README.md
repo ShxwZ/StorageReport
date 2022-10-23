9ca89f6a40cc77997f8e2d88fa49ba1780eb6cac
BuildTools <unconfigured@null.spigotmc.org>
2022-10-24 01:50:53 +0200

    [+] Now create directories for report.txt

--- README.md
+++ README.md
@@ -5,5 +5,30 @@
 
 ## How it works?
 ### You write the path of the directory and then the name you want to put to the report :)
+### (24/10/2022) Now create a directory for the report!
 ### Example:
 []!()
+
+## Update (24/10/2022) ✔
+#### This method is now more reusable
+````
+    public int getNumberOfTypeArchive(File directory, int type) {
+        File [] folder = directory.listFiles();
+        int counter = 0;
+        if (folder != null) {
+            for (File file : folder) {
+                if (type == FILES){
+                    counter += file.isDirectory() ? getNumberOfTypeArchive(file,type) : 1;
+                    if (!file.isDirectory())
+                        fileList.add(file);
+                } else if (type == FOLDERS){
+                    if (file.isDirectory()) {
+                        counter += 1 + getNumberOfTypeArchive(file,type);
+                        foldersList.add(file);
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+````
--- src/StorageInfo/Storage.java
+++ src/StorageInfo/Storage.java
@@ -17,61 +17,55 @@ import java.util.List;
 public class Storage {
     List<File> fileList = new ArrayList<>();
     List<File> foldersList = new ArrayList<>();
+    final int FOLDERS = 1;
+    final int FILES = 2;
 
     /**
      * Get info from folder
      * @param route Path of folder
      */
     public void getInfoFolder(String route) {
-        getNumberOfFiles(new File(route));
-        getNumberOfFolders(new File(route));
-        createFile(route);
+        getNumberOfTypeArchive(new File(route), FILES);
+        getNumberOfTypeArchive(new File(route), FOLDERS);
+        createFile();
 
     }
 
     /**
-     * Get number of files
+     * Get number of files or folders
      * @param directory Folder
-     * @return int: number of files
+     * @param type FOLDERS or FILES
+     * @return int: number of files or folders
      */
-    public int getNumberOfFiles(File directory) {
+    public int getNumberOfTypeArchive(File directory, int type) {
         File [] folder = directory.listFiles();
         int counter = 0;
         if (folder != null) {
             for (File file : folder) {
-                counter += file.isDirectory() ? getNumberOfFiles(file) : 1;
-                if (!file.isDirectory())
-                    fileList.add(file);
-            }
-        }
-        return counter;
-    }
-
-    /**
-     * Get number of folders
-     * @param directory Folder
-     * @return int: count of folders
-     */
-    public int getNumberOfFolders(File directory) {
-        File [] folder = directory.listFiles();
-        int counter = 0;
-        if (folder != null) {
-            for (File file : folder) {
-                if (file.isDirectory()) {
-                    counter += 1 + getNumberOfFolders(file);
-                    foldersList.add(file);
+                if (type == FILES){
+                    counter += file.isDirectory() ? getNumberOfTypeArchive(file,type) : 1;
+                    if (!file.isDirectory())
+                        fileList.add(file);
+                } else if (type == FOLDERS){
+                    if (file.isDirectory()) {
+                        counter += 1 + getNumberOfTypeArchive(file,type);
+                        foldersList.add(file);
+                    }
                 }
             }
         }
         return counter;
     }
 
+
     /**
      * Create file with information of the folder
-     * @param route Path of folder
      */
-    public void createFile(String route) {
-        File file = new File(route + "/" + InputManager.getStringNotEmpty("nombre para el informe") + ".txt");
+    public void createFile() {
+        String routeReport = InputManager.getStringNotEmpty("el directorio para el informe");
+        File newFolder = new File(routeReport);
+        if (!newFolder.exists()) newFolder.mkdirs();
+        File file = new File(routeReport + "/" + InputManager.getStringNotEmpty("nombre para el informe") + ".txt");
         List<File> files = new ArrayList<>(fileList);
         List<File> folders = new ArrayList<>(foldersList);
         try (PrintWriter newArchive = new PrintWriter(new FileWriter(file))){
@@ -83,12 +77,12 @@ public class Storage {
             newArchive.println("");
             newArchive.println("==[Listado de carpetas]==");
             folders.forEach(folder -> newArchive.printf("Nombre carpeta: %-15s Path: %-65s Número archivos: %d\n",
-                                                        folder.getName(),folder.getPath(),getNumberOfFiles(folder)));
+                                                        folder.getName(),folder.getPath(), getNumberOfTypeArchive(folder,FILES)));
             newArchive.println("");
             newArchive.println("==[Listado de archivos]==");
             files.forEach(file_ -> newArchive.printf("Nombre: %-30s Path: %-65s\n" ,
                                                     file_.getName(),file_.getPath()));
-            System.out.println("[+] Informe generado en " + route + "!");
+            System.out.println("[+] Informe generado en " + routeReport + "!");
         } catch (IOException e) {
             System.err.println("[ERROR] El path introducido no es valido!");
         }
